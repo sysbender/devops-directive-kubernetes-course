c587cf34cb17eda0d13d7d9639e6f2ace06abc2e
Jason <sysbender@gmail.com>
2024-12-27 22:16:14 +0000

    rs

---
+++ tf/main.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+      kubernetes = {
+        source = "hashicorp/kubernetes"
+        version = "2.23.0"
+      }
+    }
+
+
+
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
